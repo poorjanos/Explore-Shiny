76f6676781c7e7da2bf0f30329bce20d49f5ba2f
Janos Poor <poorjanos@gmail.com>
2018-03-27 13:01:24 +0200

    Add basic functionality

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
@@ -10,15 +12,68 @@ ui <- fluidPage(titlePanel("BC Liquor Store prices"),
                       "Price",
                       min = 0,
                       max = 100,
-                      value = c(25, 40),
-                      pre = "$"
+                      value = c(10, 90),
+                       pre = "$"
+                    )
+                    ,
+                    radioButtons(
+                      "typeInput",
+                      "Product type",
+                      choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                      selected = "WINE"
+                    ),
+                    selectInput(
+                      "countryInput",
+                      "Country",
+                      choices = c("CANADA", "FRANCE", "ITALY")
                     )
                   ),
-                  mainPanel("the results will go here")
+                  mainPanel(h2(textOutput("elemnum")),
+                            plotOutput("coolplot"),
+                            br(),
+                            br(),
+                            tableOutput("results"))
                 ))
 
 
 server <- function(input, output) {
   
+  output$elemnum <- renderText({
+    filtered <-
+      bcl %>%
+      filter(
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type == input$typeInput,
+        Country == input$countryInput
+      )
+    paste("Elemszám", dim(filtered)[1])
+  })
+  
+  output$coolplot <- renderPlot({
+    filtered <-
+      bcl %>%
+      filter(
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type == input$typeInput,
+        Country == input$countryInput
+      )
+    ggplot(filtered, aes(Alcohol_Content)) +
+      geom_histogram()
+  })
+  
+  output$results <- renderTable({
+    filtered <-
+      bcl %>%
+      filter(
+        Price >= input$priceInput[1],
+        Price <= input$priceInput[2],
+        Type == input$typeInput,
+        Country == input$countryInput
+      )
+    filtered
+  })
 }
+
 shinyApp(ui = ui, server = server)
